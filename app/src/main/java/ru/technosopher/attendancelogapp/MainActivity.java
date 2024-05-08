1f1f44ae2abaefeda551699f13c188e804a05d8e
Axtemka <115342104+Axtemka@users.noreply.github.com>
2024-05-08 23:20:36 +0300

    added bottom navigation bar, main fragments navigation

--- app/src/main/java/ru/technosopher/attendancelogapp/MainActivity.java
+++ app/src/main/java/ru/technosopher/attendancelogapp/MainActivity.java
@@ -1,19 +1,41 @@
 package ru.technosopher.attendancelogapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 
-//TODO(PROJECT...)
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+import ru.technosopher.attendancelogapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ChipNavigationBar navigationBar;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("123");
-        System.out.println("testarem oeuv0ehvoerv[");
-        System.out.println("12345");
-        System.out.println("oub[gf2q9gfp2fb[2buf[v'2enf");
+
+        navigationBar = findViewById(R.id.bottom_navigation_bar);
+
+        navigationBar.setOnItemSelectedListener(i -> {
+
+            navController = Navigation.findNavController(MainActivity.this, R.id.fragmentContainerView);
+
+            if (i == R.id.lessons) {
+                navController.navigate(R.id.lessonsFragment);
+            }
+            if (i == R.id.groups) {
+                navController.navigate(R.id.groupsFragment);
+            }
+            if (i == R.id.profile) {
+                navController.navigate(R.id.profileFragment);
+            }
+        });
+
     }
 }
