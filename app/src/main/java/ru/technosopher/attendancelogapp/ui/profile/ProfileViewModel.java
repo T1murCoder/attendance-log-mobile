1c6bf055b7571a21e62c0bc73c66e1b98497cb63
Axtemka <115342104+Axtemka@users.noreply.github.com>
2024-07-19 18:32:00 +0300

    features image background loading

--- app/src/main/java/ru/technosopher/attendancelogapp/ui/profile/ProfileViewModel.java
+++ app/src/main/java/ru/technosopher/attendancelogapp/ui/profile/ProfileViewModel.java
@@ -1,5 +1,6 @@
 package ru.technosopher.attendancelogapp.ui.profile;
 
+import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.util.Log;
 
@@ -11,83 +12,130 @@ import androidx.lifecycle.ViewModel;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import ru.technosopher.attendancelogapp.data.repository.ImageRepositoryImpl;
 import ru.technosopher.attendancelogapp.data.repository.TeacherRepositoryImpl;
-import ru.technosopher.attendancelogapp.domain.teacher.GetTeacherByIdUseCase;
+import ru.technosopher.attendancelogapp.domain.entities.ImageEntity;
 import ru.technosopher.attendancelogapp.domain.entities.TeacherEntity;
+import ru.technosopher.attendancelogapp.domain.images.UploadProfileImageUseCase;
 import ru.technosopher.attendancelogapp.domain.sign.LogoutUseCase;
+import ru.technosopher.attendancelogapp.domain.teacher.GetTeacherByIdUseCase;
 import ru.technosopher.attendancelogapp.domain.teacher.UpdateTeacherProfileUseCase;
 
 public class ProfileViewModel extends ViewModel {
-    public static final String TAG = "PROFILE_VIEW_MODEL";
-    public static final String AVATAR_PREFIX = "images/avatar_";
-    private final FirebaseStorage storage = FirebaseStorage.getInstance();
-    private StorageReference storageRef = storage.getReference();
-    private final MutableLiveData<State> mutableStateLiveData = new MutableLiveData<>();
 
+    public static final String TAG = "PROFILE_VIEW_MODEL";
+
+    private final MutableLiveData<State> mutableStateLiveData = new MutableLiveData<>();
     public final LiveData<State> stateLiveData = mutableStateLiveData;
+
     private final MutableLiveData<Void> mutableLogoutLiveData = new MutableLiveData<>();
     public final LiveData<Void> logoutLiveData = mutableLogoutLiveData;
+
     private final LogoutUseCase logoutUseCase = new LogoutUseCase(
             TeacherRepositoryImpl.getInstance()
     );
+
+    private final GetTeacherByIdUseCase getUserByIdUseCase = new GetTeacherByIdUseCase(
+            TeacherRepositoryImpl.getInstance()
+    );
+
     private final UpdateTeacherProfileUseCase updateTeacherProfileUseCase = new UpdateTeacherProfileUseCase(
             TeacherRepositoryImpl.getInstance()
     );
-    private final GetTeacherByIdUseCase getTeacherByIdUseCase = new GetTeacherByIdUseCase(
-            TeacherRepositoryImpl.getInstance()
+
+    private final UploadProfileImageUseCase uploadProfileImageUseCase = new UploadProfileImageUseCase(
+            ImageRepositoryImpl.getINSTANCE()
     );
+
+    private final FirebaseStorage storage = FirebaseStorage.getInstance();
+    private StorageReference storageRef = storage.getReference();
+
+    @Nullable
+    private String id;
+    @Nullable
+    private String login;
     @Nullable
     private String name;
+
     @Nullable
     private String surname;
+
     @Nullable
     private String telegram;
+
     @Nullable
     private String github;
+
     @Nullable
     private String photo;
 
     public void loadPrefs(String id, String prefsLogin, String prefsName, String prefsSurname, String prefsTelegram, String prefsGithub, String prefsPhotoUrl) {
-        getTeacherByIdUseCase.execute(id, teacher -> {
-            if (teacher == null) {
-                changeName(prefsName);
-                changeSurname(prefsSurname);
-                changeTelegram(prefsTelegram);
-                changeGithub(prefsGithub);
-                changePhoto(prefsPhotoUrl);
-                mutableStateLiveData.postValue(new State(null, new TeacherEntity(
-                        id,
-                        prefsName,
-                        prefsSurname,
-                        prefsLogin,
-                        prefsTelegram,
-                        prefsGithub,
-                        prefsPhotoUrl
-                ), false));
-
-            } else {
-                changeName(teacher.getValue().getName());
-                changeSurname(teacher.getValue().getSurname());
-                changeTelegram(teacher.getValue().getTelegram_url());
-                changeGithub(teacher.getValue().getGithub_url());
-                changePhoto(teacher.getValue().getPhoto_url());
-
-                mutableStateLiveData.postValue(new State(
-                        null, new TeacherEntity(
-                        id,
-                        teacher.getValue().getName(),
-                        teacher.getValue().getSurname(),
-                        teacher.getValue().getUsername(),
-                        teacher.getValue().getTelegram_url(),
-                        teacher.getValue().getGithub_url(),
-                        teacher.getValue().getPhoto_url()
-                ), false
-                ));
-
-            }
-        });
-
+        changeId(id);
+        changeName(prefsName);
+        changeSurname(prefsSurname);
+        changeTelegram(prefsTelegram);
+        changeGithub(prefsGithub);
+        changePhoto(prefsPhotoUrl);
+        changeLogin(prefsLogin);
     }
+
+    public void update(String id, String prefsLogin, String prefsName, String prefsSurname, String prefsTelegram, String prefsGithub, String prefsPhotoUrl) {
+        loadPrefs(id, prefsLogin, prefsName, prefsSurname, prefsTelegram, prefsGithub, prefsPhotoUrl);
+        mutableStateLiveData.postValue(new State(null, new TeacherEntity(
+                id,
+                name,
+                surname,
+                login,
+                telegram,
+                github,
+                photo
+        ), false));
+    }
+
+    private void update(String photoUrl) {
+        loadPrefs(id, login, name, surname, telegram, github, photoUrl);
+        mutableStateLiveData.postValue(new State(null, new TeacherEntity(
+                id,
+                name,
+                surname,
+                login,
+                telegram,
+                github,
+                photoUrl
+        ), false));
+    }
+
+    @SuppressLint("CheckResult")
+    public void uploadAvatar(String id, Uri image) {
+        if (image != null) {
+            mutableStateLiveData.postValue(new State("Загрузка...", null, false));
+            uploadProfileImageUseCase.execute(id, image).
+                    subscribeOn(Schedulers.io())
+                    .subscribe((photoUrl, throwable) -> {
+                        if (throwable != null) {
+                            mutableStateLiveData.postValue(new State("Не удалось загрузить аватар!", null, false));
+                        } else {
+                            Log.d(TAG, "Запрос сделан!");
+                            updateTeacherProfileUseCase.execute(
+                                    id,
+                                    new TeacherEntity(
+                                            id,
+                                            null,
+                                            null,
+                                            null,
+                                            null,
+                                            null,
+                                            photoUrl
+                                    ),
+                                    userStatus -> update(photoUrl));
+                        }
+                    });
+        } else {
+            mutableStateLiveData.postValue(new State("Пожалуйста, выберите изображение", null, false));
+        }
+    }
+
     public void updateProfile(String id, String prefsLogin) {
         if (name == null || surname == null || name.isEmpty() || surname.isEmpty()) {
             mutableStateLiveData.postValue(new State("Имя и фамилия не могут быть пустыми", null, false));
@@ -103,80 +151,55 @@ public class ProfileViewModel extends ViewModel {
                             telegram,
                             github,
                             photo),
-                    tstatus -> {
-                        if (tstatus.getStatusCode() == 200) {
-                            loadPrefs(id, prefsLogin, name, surname, telegram, github, photo);
+                    userstatus -> {
+                        if (userstatus.getStatusCode() == 200) {
+                            update(id, prefsLogin, name, surname, telegram, github, photo);
                         } else {
-                            Log.e("ProfileViewModel", "" + tstatus.getStatusCode());
                             mutableStateLiveData.postValue(new State("Что то пошло не так. Попробуйте еще раз", null, false));
                         }
                     });
         }
     }
-    public void uploadAvatar(String id, String prefsLogin, Uri image) {
-        if (image != null) {
-            StorageReference imageRef = storageRef.child(AVATAR_PREFIX + id + ".png");
 
-            imageRef.putFile(image).addOnSuccessListener(taskSnapshot -> {
-                Log.d(TAG, "Image loaded!");
-                updateTeacherProfileUseCase.execute(
-                        id,
-                        new TeacherEntity(
-                                id,
-                                null,
-                                null,
-                                null,
-                                null,
-                                null,
-                                imageRef.getPath()
-                        ),
-                        userStatus -> loadPrefs(id, prefsLogin, name, surname, telegram, github, imageRef.getPath())
-                );
-            }).addOnFailureListener(e -> {
-                Log.d(TAG, e.toString());
-                mutableStateLiveData.postValue(new State("Не получилось загрузить аватар!", null, false));
-            });
-        } else {
-            Log.d(TAG, "Image is null!");
-        }
+    public void changeName(String name){
+        this.name = name.trim();
     }
+    public void changeSurname(String surname){
+        this.surname = surname.trim();
+    }
+    public void changeTelegram(String telegram){
+        this.telegram = telegram;
+    }
+    public void changeGithub(String github){
+        this.github = github;
+    }
+    public void changePhoto(String photo) {this.photo = photo;}
+    public void changeLogin(String login) {this.login = login;}
+    private void changeId(String id) {this.id = id;}
+
     public void logout() {
         logoutUseCase.execute();
         mutableLogoutLiveData.postValue(null);
     }
-    public void changeName(String name) {
-        this.name = name;
-    }
-    public void changeSurname(String surname) {
-        this.surname = surname;
-    }
-    public void changeTelegram(String telegram) {
-        this.telegram = telegram;
-    }
-    public void changeGithub(String github) {
-        this.github = github;
-    }
-    public void changePhoto(String photo) {
-        this.photo = photo;
-    }
+
     public class State {
         @Nullable
         private final String errorMessage;
         @Nullable
-        private final TeacherEntity teacher;
+        private final TeacherEntity user;
 
         @Nullable
         private final Boolean loading;
 
-        public State(@Nullable String errorMessage, @Nullable TeacherEntity teacher, @Nullable Boolean loading) {
+        public State(@Nullable String errorMessage, @Nullable TeacherEntity user, @Nullable Boolean loading) {
             this.errorMessage = errorMessage;
-            this.teacher = teacher;
+            this.user = user;
             this.loading = loading;
         }
 
         @Nullable
-        public TeacherEntity getTeacher() {
-            return teacher;
+        public TeacherEntity getUser() {
+            return user;
         }
 
         @Nullable
